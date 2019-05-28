1205e8896f38edd86a3cff7b4cbee64b6332d16c
macmillan2020 <erinmacmillan@gmail.com>
2019-05-28 10:34:39 -0400

    add list container and list item component

--- src/components/AdminApplications.js
+++ src/components/AdminApplications.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PageTitle from './PageTitle';
+import ListContainer from './ListContainer';
 
 export default function AllApplications() {
   return (
     <div>
       <PageTitle title="Cohort Application Form" />
       <p>List of applications...</p>
+      <ListContainer></ListContainer>
     </div>
   );
 }

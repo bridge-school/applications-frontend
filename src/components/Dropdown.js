dd540e4287fa5c39cbe631a87d29df57b97ed0e5
macmillan2020 <erinmacmillan@gmail.com>
2019-05-30 12:57:10 -0400

    add test data to dropdown comp

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -13,16 +13,48 @@ const SelectBox = styled.select`
   background-size: 1rem auto, 100%;
 `;
 
-export default function Dropdown({ data }) {
+const data = {
+  description: 'Random Multiple choice question.',
+  type: 'checkbox',
+  items: [
+    {
+      label: 'Option a',
+      value: 'A',
+    },
+    {
+      label: 'Option b',
+      value: 'B',
+    },
+  ],
+};
+
+export default function Dropdown({}) {
+  {
+    const data = {
+      description: 'Random Multiple choice question.',
+      type: 'checkbox',
+      items: [
+        {
+          label: 'Option a',
+          value: 'A',
+        },
+        {
+          label: 'Option b',
+          value: 'B',
+        },
+      ],
+    };
+  }
   return (
     <div>
       <label>
         {data.description}
+        <br />
         <SelectBox>
           <option value="" />
           {data.items.map(item => (
             <option key={data.items.indexOf(item)} value={item.value}>
-              {item.option}
+              {item.label}
             </option>
           ))}
         </SelectBox>

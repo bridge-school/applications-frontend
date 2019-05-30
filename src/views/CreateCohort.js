61263e707261bb5faac374c92145867e50bea935
Corina Wong <hello@corinawong.com>
2019-05-30 11:29:13 -0400

    Add routing

---
+++ src/views/CreateCohort.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PageTitle from '../components/PageTitle';
+
+export default function CreateCohort() {
+  return (
+    <div>
+      <PageTitle title="Create Cohort Application Form" />
+    </div>
+  );
+}

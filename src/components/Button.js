3dd329c80ee1332227021900fd70ac7110beece8
Corina Wong <hello@corinawong.com>
2019-05-30 19:55:13 -0400

    Alter Button to be  styled by more prop options

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,22 +2,30 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const ButtonElem = styled.a`
-  display: inline-block;
-  border-radius: 3px;
-  padding: 0.5rem 0;
-  margin: 0.5rem 1rem;
-  width: 11rem;
-  font-size: 0.8rem;
-  background: #22c29d;
+const ButtonElem = styled.button`
+  border-radius: ${p => p.theme.borderRadius};
+  padding: 0.5em;
   color: white;
-  border: 2px solid #22c29d;
+  text-align: center;
+  font-weight: bold;
+  width: ${p => p.width || '24em'};
+  font-size: ${p => (p.uppercase ? '1rem' : '.8rem')};
+  background: ${p => p.backgroundColor};
+  border: 3px solid ${p => p.backgroundColor};
+  text-transform: ${p => (p.uppercase ? 'uppercase' : 'lowercase')};
+  &:hover {
+    color: ${p => p.backgroundColor};
+    background: white;
+  }
 `;
 
-export default function Button({ text }) {
-  return <ButtonElem>{text}</ButtonElem>;
+export default function Button(props) {
+  return <ButtonElem {...props}>{props.text}</ButtonElem>;
 }
 
-ButtonElem.propTypes = {
-  text: PropTypes.string,
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  backgroundColor: PropTypes.string.isRequired,
+  width: PropTypes.string,
+  uppercase: PropTypes.bool,
 };

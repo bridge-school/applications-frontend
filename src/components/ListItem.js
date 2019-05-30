653995a52ed4c885dd00de6347b1b8c290297af9
macmillan2020 <erinmacmillan@gmail.com>
2019-05-30 14:28:41 -0400

    rename components and add test data

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import Button from './Button';
 
-const ListItem = styled.li`
+const LI = styled.li`
   background-color: ${props => props.theme.grey};
   padding: ${props => props.theme.padding};
   margin: 0.2rem 0;
@@ -13,22 +14,25 @@ const ListItem = styled.li`
 const Link = styled.a`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   color: black;
   text-decoration: none;
   } */}
 `;
 
-export default function ApplicationListItem({ name }) {
+export default function ListItem({ name, type, url }) {
   return (
-    <ListItem>
-      <Link href="#">
+    <LI>
+      <Link href={'apply/' + url}>
         {name}
-        <div>button component...</div>
+        <Button text={type} />
       </Link>
-    </ListItem>
+    </LI>
   );
 }
 
-ApplicationListItem.propTypes = {
+ListItem.propTypes = {
   name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
 };

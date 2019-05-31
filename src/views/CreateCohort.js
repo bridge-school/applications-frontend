2a0bbd5798c5b88044d25d90a9d22c47cb82e090
Priyanka Patel <ppatel@evokesolutions.com>
2019-05-30 20:28:55 -0400

    Input and Textarea

--- src/views/CreateCohort.js
+++ src/views/CreateCohort.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PageTitle from '../components/PageTitle';
 import InputDate from '../components/InputDate';
+import Input from '../components/Input';
 
 export default function CreateCohort() {
   return (
@@ -8,6 +9,8 @@ export default function CreateCohort() {
       <PageTitle title="Create Cohort Application Form" />
       <form action="" method="post">
         <InputDate name="test" value="" label="Date Input" />
+        <Input name="text-input" type="text" value="" label="Text Input" />
+        <Input name="textarea-input" type="textarea" value="" label="Textarea Input" />
       </form>
     </div>
   );

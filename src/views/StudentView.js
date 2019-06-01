90e6eaae52ee5c0794140320608b7aeb3084642e
macmillan2020 <erinmacmillan@gmail.com>
2019-06-01 08:14:12 -0400

    add radio button component

--- src/views/StudentView.js
+++ src/views/StudentView.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import PageTitle from '../components/PageTitle';
+import Radio from '../components/Radio';
 
 export default function StudentView() {
   return (
     <div>
       <PageTitle title="Cohort Application Forms" />
       <p>STUDENT VIEW...</p>
+      <Radio
+        name="pronouns"
+        description="What pronouns should we use?"
+        items={['He/Him', 'She/Her', 'They/Them']}
+        value=""
+      />
     </div>
   );
 }

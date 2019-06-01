c91d0cab1768f0400c0d8373d5a48a878b29035b
macmillan2020 <erinmacmillan@gmail.com>
2019-06-01 10:34:57 -0400

    remove button component

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import Button from './Button';
 
 const LI = styled.li`
   background-color: ${props => props.theme.grey};

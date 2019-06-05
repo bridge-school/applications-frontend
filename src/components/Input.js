958ff2c0ab4ac605f79c94394ef6628205720774
Corina Wong <hello@corinawong.com>
2019-06-05 11:40:10 -0400

    Add required indicator for input

--- src/components/Input.js
+++ src/components/Input.js
@@ -7,7 +7,8 @@ const InputElement = styled.input`
   margin: ${props => props.theme.margin} 0;
   color: ${props => props.theme.black};
   background: ${props => props.theme.inputBackground};
-  border: ${props => props.theme.borderWidth} solid ${props => props.theme.borderColor};
+  border: ${props => props.theme.borderWidth} solid
+    ${props => props.theme.borderColor};
   border-radius: ${props => props.theme.borderRadius};
   display: block;
   width: 100%;
@@ -18,33 +19,45 @@ const TextareaElement = styled.textarea`
   margin: ${props => props.theme.margin} 0;
   color: ${props => props.theme.black};
   background: ${props => props.theme.inputBackground};
-  border: ${props => props.theme.borderWidth} solid ${props => props.theme.borderColor};
+  border: ${props => props.theme.borderWidth} solid
+    ${props => props.theme.borderColor};
   border-radius: ${props => props.theme.borderRadius};
   display: block;
   border-radius: ${props => props.theme.borderRadius};
   width: 100%;
 `;
 
-export default function Input({ label, name, type, value, handleChange }) {
+export default function Input({
+  label,
+  name,
+  type,
+  value,
+  handleChange,
+  required,
+}) {
   return (
     <div>
       <label htmlFor={name}>
         {label}
-        {type === 'textarea' 
-          ? <TextareaElement 
-              id={name} 
-              name={name}
-              value={value}
-              rows="3"
-              onChange={e => handleChange(e)} />
-          : <InputElement 
-              id={name} 
-              name={name} 
-              type={type} 
-              value={value}
-              onChange={e => handleChange(e)}
-            />
-        }
+        {required && '*'}
+        {type === 'textarea' ? (
+          <TextareaElement
+            id={name}
+            name={name}
+            value={value}
+            rows="3"
+            onChange={e => handleChange(e)}
+          />
+        ) : (
+          <InputElement
+            id={name}
+            name={name}
+            type={type}
+            required={required}
+            value={value}
+            onChange={e => handleChange(e)}
+          />
+        )}
       </label>
     </div>
   );
@@ -55,5 +68,5 @@ Input.propTypes = {
   name: PropTypes.string,
   type: PropTypes.string,
   value: PropTypes.string,
-  handleChange: PropTypes.func
+  handleChange: PropTypes.func,
 };

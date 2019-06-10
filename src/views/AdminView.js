b8066174987ed85c1c3635b34cd91f238b4fce0a
Corina Wong <hello@corinawong.com>
2019-06-10 17:48:58 -0400

    Connect Redux to admin view

--- src/views/AdminView.js
+++ src/views/AdminView.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PageTitle from '../components/PageTitle';
 import styled from 'styled-components';
-import { useFetch } from '../hooks/fetch';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import ListContainer from '../components/ListContainer';
+import { fetchAllCohorts } from '../store/actions';
 
 const Header = styled.header`
   display: flex;
@@ -12,9 +14,19 @@ const Header = styled.header`
   margin: 1.5rem 0 3.5rem;
 `;
 
-export default function AdminView() {
-  const url = 'http://applications-backend.bridgeschoolapp.io/applications';
-  const [data, loading] = useFetch(url);
+function AdminView({ error, allCohorts, loading, getAllCohorts }) {
+  useEffect(() => {
+    getAllCohorts();
+  }, [getAllCohorts]);
+
+  if (error) {
+    return <div>Error! {error.message}</div>;
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <Header>
@@ -23,7 +35,31 @@ export default function AdminView() {
           Create application group
         </Link>
       </Header>
-      {loading ? <div>loading</div> : <ListContainer cohortData={data} />}
+      <ListContainer cohortData={allCohorts} />
     </div>
   );
 }
+
+const mapStateToProps = state => ({
+  error: state.error,
+  loading: state.loading,
+  allCohorts: state.allCohorts,
+});
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getAllCohorts: () => dispatch(fetchAllCohorts()),
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AdminView);
+
+AdminView.propTypes = {
+  error: PropTypes.object,
+  allCohorts: PropTypes.array,
+  loading: PropTypes.bool,
+  getAllCohorts: PropTypes.func,
+};

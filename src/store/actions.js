c5c80de51a9ee9e4550d1a26f9c2f251a0874366
Corina Wong <hello@corinawong.com>
2019-06-10 20:08:25 -0400

    [Chore] Fetch current cohort data from db/Redux (#52)
    * Fetch current cohort data from db/Redux
    
    * Move Redux Provider into App component to satisfy test

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,10 +3,10 @@ export const actionType = {
   CREATE_COHORT_REQUEST: 'CREATE_COHORT_REQUEST',
   CREATE_COHORT_SUCCESS: 'CREATE_COHORT_SUCCESS',
   CREATE_COHORT_ERROR: 'CREATE_COHORT_ERROR',
-  FETCH_COHORT: 'FETCH_COHORT',
-  FETCH_CURRENT_COHORTS: 'FETCH_CURRENT_COHORTS',
   FETCH_ALL_COHORTS_REQUEST: 'FETCH_ALL_COHORTS_REQUEST',
   FETCH_ALL_COHORTS_SUCCESS: 'FETCH_ALL_COHORTS_SUCCESS',
+  FETCH_CURRENT_COHORTS_REQUEST: 'FETCH_CURRENT_COHORTS_REQUEST',
+  FETCH_CURRENT_COHORTS_SUCCESS: 'FETCH_CURRENT_COHORTS_SUCCESS',
 
   STUDENT_SUBMISSION: 'STUDENT_SUBMISSION',
 };
@@ -25,16 +25,13 @@ export const createCohortRequestError = err => ({
   type: actionType.CREATE_COHORT_ERROR,
   payload: err,
 });
-
 export const createCohortRequest = () => ({
   type: actionType.CREATE_COHORT_REQUEST,
 });
-
 export const createCohortSuccess = createdID => ({
   type: actionType.CREATE_COHORT_SUCCESS,
   payload: createdID,
 });
-
 export const createCohort = formData => dispatch => {
   dispatch(createCohortRequest());
 
@@ -62,12 +59,10 @@ export const createCohort = formData => dispatch => {
 export const fetchAllCohortsRequest = () => ({
   type: actionType.FETCH_ALL_COHORTS_REQUEST,
 });
-
 export const fetchAllCohortsSuccess = allCohorts => ({
   type: actionType.FETCH_ALL_COHORTS_SUCCESS,
   payload: allCohorts,
 });
-
 export const fetchAllCohorts = () => dispatch => {
   dispatch(fetchAllCohortsRequest());
 
@@ -79,5 +74,24 @@ export const fetchAllCohorts = () => dispatch => {
       return res.json();
     })
     .then(res => dispatch(fetchAllCohortsSuccess(res.data)))
+    .catch(err => dispatch(error(err)));
+};
+
+export const fetchCurrentCohortsRequest = () => ({
+  type: actionType.FETCH_CURRENT_COHORTS_REQUEST,
+});
+export const fetchCurrentCohortsSuccess = currentCohorts => ({
+  type: actionType.FETCH_CURRENT_COHORTS_SUCCESS,
+  payload: currentCohorts,
+});
+export const fetchCurrentCohorts = () => dispatch => {
+  dispatch(fetchCurrentCohortsRequest());
+
+  fetch(`${BASE_URL}/applications/current`)
+    .then(res => {
+      if (!res.ok) throw new Error(res.statusText);
+      return res.json();
+    })
+    .then(res => dispatch(fetchCurrentCohortsSuccess(res.data)))
     .catch(err => dispatch(error(err)));
 };
--- src/views/StudentView.js
+++ src/views/StudentView.js
@@ -1,23 +1,58 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import PageTitle from '../components/PageTitle';
 import ListContainer from '../components/ListContainer';
-import { useFetch } from '../hooks/fetch';
 import styled from 'styled-components';
+import { fetchCurrentCohorts } from '../store/actions';
 
 const Header = styled.header`
   margin: 0 0 3rem 0;
 `;
 
-export default function StudentView() {
-  const url =
-    'http://applications-backend.bridgeschoolapp.io/applications/current';
-  const [data, loading] = useFetch(url);
+function StudentView({ error, loading, getCurrentCohorts, currentCohorts }) {
+  useEffect(() => {
+    getCurrentCohorts();
+  }, [getCurrentCohorts]);
+
+  if (error) {
+    return <div>Error! {error.message}</div>;
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <Header>
         <PageTitle title="Cohort Application Forms" />
       </Header>
-      {loading ? <div>loading</div> : <ListContainer cohortData={data} />}
+      <ListContainer cohortData={currentCohorts} />
     </div>
   );
 }
+
+const mapStateToProps = state => ({
+  error: state.error,
+  loading: state.loading,
+  currentCohorts: state.currentCohorts,
+});
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getCurrentCohorts: () => dispatch(fetchCurrentCohorts()),
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(StudentView);
+
+StudentView.propTypes = {
+  error: PropTypes.object,
+  currentCohorts: PropTypes.array,
+  loading: PropTypes.bool,
+  getCurrentCohorts: PropTypes.func,
+};

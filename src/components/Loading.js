8d00095204375be1525c04658733e7ddcb7cd04f
Priyanka Patel <ppatel@evokesolutions.com>
2019-06-18 12:43:43 -0400

    Global Loading style and functionality on Login view

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import loading from '../images/loading.svg';
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100%;
+  height: calc(100% - 100px);
+  left: 0;
+  position: absolute;
+  display: block;
+  background: rgba(255, 255, 255, 0.8);
+  z-index: 10;
+`;
+
+const LoadingIcon = styled.span`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  height: 48px;
+  width: 48px;
+  display: block;
+  background-image: url(${loading});
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center center;
+`;
 
 export default function Loading() {
   return (
     <Container>
-      <p>Loading...</p>
+      <LoadingIcon />
     </Container>
   );
 }
